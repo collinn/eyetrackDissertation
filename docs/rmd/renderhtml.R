316e520ccccaf023850afb75086f8cf6872a1acd
collin <collin@uiowa.edu>
2022-03-24 14:54:26 -0500

    lkajsdflkajs

---
+++ docs/rmd/renderhtml.R
@@ -0,0 +1,10 @@
+
+## Compile rmd documents to correct place
+library(rmarkdown)
+
+render("index.Rmd", output_dir = "../")
+render("old.Rmd", output_dir = "../")
+
+render("bdots.Rmd", output_dir = "../subdir/")
+render("eyetrackSim.Rmd", output_dir = "../subdir/")
+render("theory.Rmd", output_dir = "../subdir/")

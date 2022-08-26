20d98ef5e8f1f972f5878a0301710d0f0558a9ae
collin <collin_nolte@uiowa.edu>
2022-08-26 16:32:48 -0500

    spline stuff closer to figuring out

--- analysis/splines_construct.R
+++ analysis/splines_construct.R
@@ -20,3 +20,26 @@ x <- seq(0, 1, 0.1)
 bsMat <- bSpline(x, knots = knots, degree = 2, intercept = TRUE)
 matplot(x, bsMat, type = 'l', ylab = 1)
 abline(v = knots, lty = 2, col = 'gray')
+
+
+y <- seq(-2, 2, by = 0.1)^2
+
+ysp <- ns(y, df = 3)
+matplot(ysp, type = 'l', col = 'gray50', ylim = c(-0.5, 2))
+lines(rowSums(ysp))
+lines(y)
+abline(v = 16, col = 'red', lty = 2)
+fit <- lm(y ~ ysp)
+
+
+require(stats); require(graphics)
+nsw <- ns(women$height, df = 5)
+summary(fm1 <- lm(weight ~ ns(height, df = 5), data = women))
+attr(terms(fm1), "predvars")
+
+## example of safe prediction
+plot(women, xlab = "Height (in)", ylab = "Weight (lb)")
+ht <- seq(57, 73, length.out = 200) ; nD <- data.frame(height = ht)
+lines(ht, p1 <- predict(fm1, nD))
+stopifnot(all.equal(p1, predict(update(fm1, . ~
+                                         splines::ns(height, df=5)), nD)))

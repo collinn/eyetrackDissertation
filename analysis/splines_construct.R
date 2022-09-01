c775275f1387d785b70bd1966359d429cc4a9833
collin <collin_nolte@uiowa.edu>
2022-09-01 13:44:01 -0500

    motherfucking PERMS!

--- analysis/splines_construct.R
+++ analysis/splines_construct.R
@@ -49,15 +49,23 @@ stopifnot(all.equal(p1, predict(update(fm1, . ~
 ## note, this doesn't actually use any splines, probably doesn't
 # even belong here
 
+library(MASS)
+library(mvtnorm)
+library(bdots)
+
 #' @param p height of step
 s1 <- function(p) {
-  stopifnot(length(p) == 2)
+  stopifnot(length(p) == 3)
+  #if (length(p) != 3) browser()
+  #print(p)
   x <- c(20, 60) # putting here for now
   x1 <- x[1]
   x2 <- x[2]
+  x3 <- p[3]
   y <- vector("numeric", length = 100L)
   y[`:`(x1, x1+15)] <- p[1]
   y[`:`(x2, x2+15)] <- p[2]
+  y[c(1:19, 36:59, 76:100)] <- x3
   y
 }
 
@@ -66,7 +74,7 @@ p <- rmvnorm(100, mean = c(1,2), sigma = diag(2)*0.25)
 
 # no need for trials
 createStepData <- function(n = 25, trials = 100, pars, gp = "A") {
-  p <- rmvnorm(n, mean = pars, sigma = diag(length(pars))*0.25)
+  p <- rmvnorm(n, mean = pars, sigma = diag(length(pars))*0.05)
   p <- abs(p)
   spars <- split(p, row(p))
   dts <- lapply(seq_len(n), function(x) {
@@ -76,13 +84,14 @@ createStepData <- function(n = 25, trials = 100, pars, gp = "A") {
                      group = gp, 
                      true = s1(pp))
     dt[, fixations := mean(rbinom(trials, 1, 0.1)) + true, by = time]
+    dt[, fixations := abs(fixations)]
   })
   dts <- rbindlist(dts)
   return(dts) # don't need pars here
 }
 
-dt1 <- createStepData(n = 25, pars = c(1,2), gp = "A")
-dt2 <- createStepData(n = 25, pars = c(0,0), gp = "B")
+dt1 <- createStepData(n = 25, pars = c(0.5,1,0), gp = "A")
+dt2 <- createStepData(n = 25, pars = c(0,0,0), gp = "B")
 dt2[, id := id + 25]
 dt <- rbindlist(list(dt1, dt2))
 
@@ -114,7 +123,7 @@ fit <- bdotsFit(data = dt,
                 group = "group",
                 y = "fixations", 
                 curveType = stepPars())
-debugonce(bdotsFit)
+# debugonce(bdotsFit)
 
 
 boot <- bdotsBoot(fixations ~ group(A, B), fit)
@@ -127,22 +136,55 @@ plot(boot, plotDiffs = FALSE)
 # less the case, say with more continuous bumps, and then see where
 # the line is drawn for bdots and permutation test
 
+getT_step <- function(x, idx, whole = FALSE) {
+  x$group <- x$group[idx]
+  fit_s <- split(x, by = "group")
+  mvl <- lapply(fit_s, function(y) {
+    cc <- coef(y)
+    time <- seq(0, 100, by = 1)
+    cl <- apply(cc, 1, function(kk) {s1(kk)})
+    mm <- rowMeans(cl)
+    vv <- apply(cl, 1, var)
+    vvn <- vv/nrow(cc)
+    list(mean = mm, nvar = vvn)
+  })
+  
+  ## Guess I don't need a function for this
+  x <- mvl[[1]]; y <- mvl[[2]]
+  xm <- x$mean; xv <- x$nvar
+  ym <- y$mean; yv <- y$nvar
+  Tt <- abs(xm-ym) / sqrt(yv + xv)
+  
+  ifelse(whole, return(Tt), return(max(Tt)))
+}
 
-## Can maybe still do splines?
-dat <- split(dt, by = "id")[[1]]
+getTDist_step <- function(x, P = 500) {
+  n <- nrow(x)
+  permmat <- replicate(P, sample(seq_len(n), n))
+  tvec <- getT_step(x, seq_len(n), whole = TRUE)
+  tnull <- apply(permmat, 2, function(y) {
+    getT_step(x, y)
+  })
+  tnull
+}
 
-tt <- ns(dat$fixations, df = 3)
+t500 <- getTDist_step(fit, P = 500)
+actual_t <- getT_step(fit, seq_len(nrow(fit)), whole = TRUE)
+tq <- quantile(t500, probs = 0.975)
 
-fit <- lm(dat$fixations ~ ns(dat$fixations, df = 3))
+plot(actual_t, type = 'l')
+abline(h = tq, lty = 2, col = 'red')
 
-plot(tt %*% coef(fit)[-1] + coef(fit)[1], type = 'l')
+time <- 1:100
+idx <- which(actual_t > tq)
+sig_time <- time[idx]
 
+lq <- which(diff(idx) != 1)
 
+abline(v = min(idx), col = 'red', lty = 2) # use idx bc curves on idx scale not time
+abline(v = max(idx), col = 'red', lty = 2)
 
-
-
-
-
-
-
-
+if (length(lq) == 1) {
+  abline(v = idx[lq], col = 'red', lty = 2)
+  abline(v = idx[lq+1], col = 'red', lty = 2)
+}

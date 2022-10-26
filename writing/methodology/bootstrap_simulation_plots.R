5a8ace06edd3d5534297b14ccc279fdd012a6f5c
collin <collin_nolte@uiowa.edu>
2022-10-26 15:45:41 -0500

    first draft at plots

---
+++ writing/methodology/bootstrap_simulation_plots.R
@@ -0,0 +1,60 @@
+
+library(data.table)
+library(bdots)
+library(eyetrackSim)
+library(mvtnorm)
+library(ggplot2)
+
+## These are probably wrong
+results <- readRDS("../data/coverage_results.rds")
+res_bdots <- readRDS("../data/bdots_coverage_results.rds")
+
+pp <- lapply(results, `[[`, 1)
+ppb <- lapply(res_bdots, `[[`, 1)
+cc <- lapply(results, `[[`, 2)
+ccb <- lapply(res_bdots, `[[`, 2)
+
+## Parameters first, let's say 90 percent coverage
+getParCoverage <- function(pp) {
+  pp <- lapply(pp, function(x) {
+    parCoverage <- apply(x, 2, function(y) sum(y > 0.05 & y < 0.95) / length(y))
+  })
+  
+  mini <- lapply(pp, `[[`, 1) |> unlist()
+  peak <- lapply(pp, `[[`, 2) |> unlist()
+  slope <- lapply(pp, `[[`, 3) |> unlist()
+  cross <- lapply(pp, `[[`, 4) |> unlist()
+  
+  return(list(mini = mini, peak = peak, slope = slope, cross = cross))
+}
+
+pp <- getParCoverage(pp)
+ppb <- getParCoverage(ppb)
+
+## Make these bitches into dts
+createDtfromPar <- function(p, simtype) {
+  dtp <- data.table(val = unlist(p, use.names = FALSE))
+  nn <- tstrsplit(names(unlist(p)), "\\.")
+  dtp$par <- nn[[1]]
+  tt <- vector("numeric", length = length(nn[[2]]))
+  qq <- as.numeric(nn[[2]])
+  for (i in seq_along(qq)) {
+    tt[i] <- switch(qq[i], 10, 25, 50, 75, 100)  
+  }
+  dtp$trial <- tt
+  dtp$sim <- simtype
+  dtp
+}
+
+dt1 <- createDtfromPar(pp, "bootstrap")
+dt2 <- createDtfromPar(ppb, "bdots")
+dt <- rbindlist(list(dt1, dt2))
+
+ggplot(data = dt, aes(x = trial, y = val, color = sim)) +
+  geom_hline(yintercept = 0.9, linetype = "dashed", color = "red") +
+  geom_line(linetype = "solid") + geom_point() +
+  facet_wrap(~par) + xlab("Trial") + ylab("Coverage") +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.1)) +
+  scale_x_continuous(breaks = c(10, 25, 50, 75, 100)) +
+  ggtitle("Parameter Coverage") + theme_bw() + theme(legend.position = "bottom")
+

9ea80b176fdf912a614f802ca73ffb7da9abf691
collin <collin_nolte@uiowa.edu>
2022-12-14 15:01:32 -0600

    bob email thing

--- analysis/trace.R
+++ analysis/trace.R
@@ -2,12 +2,14 @@
 ## This all desperately needs to be looked at by bob again
 
 
-dat <- fread("../data/trace_n_things/Events-matched-sac-fix.txt")
-dat2 <- fread("../data/trace_n_things/looks-matched_human.txt")
+# dat <- fread("../data/trace_n_things/Events-matched-sac-fix.txt")
+# dat2 <- fread("../data/trace_n_things/looks-matched_human.txt")
+# 
+# 
+# ## These are things from trace that need to be scaled
+# trace <- fread("../data/trace_n_things/trace_query.txt")
 
-
-## These are things from trace that need to be scaled
-trace <- fread("../data/trace_n_things/trace_query.txt")
+looks <- fread("../data/bob_trace_data/human_looks.csv")
 
 ## Make scaling based on bob's thing
 scaler <- function(a) { #a is max activation
@@ -18,6 +20,39 @@ scaler <- function(a) { #a is max activation
   den <- 1 + w * exp(-s * (cc - a))^(1/w)
   (ss <- 1/den)
 }
+
+## Across subjects
+looks_s <- looks[, `:=`(unrelated = mean(unrelated), 
+                        target = mean(target), 
+                         cohort = mean(cohort), 
+                        rhyme = mean(rhyme)), by = time]
+looks_s[, `:=`(unrel = NULL, subject = NULL)]
+looks_s <- unique(looks_s)
+
+
+l <- as.list(looks_s[, .(unrelated, target, cohort, rhyme)])
+lucer <- function(l, tau = 3) {
+  expl <- lapply(l, function(x) {
+    exp(tau * x)
+  })
+  ss <- Reduce(`+`, expl)
+  rr <- lapply(expl, function(x) as.data.table(x / ss))
+  setNames(rr, l)
+}
+
+## With luce choice rule
+look_luce <- Reduce(`cbind`, (lucer(l)))
+names(look_luce) <- names(l)
+look_luce$time <- looks_s$time
+
+## Get max activation
+look_luce[, maxact := max(unrelated, target, cohort, rhyme), by = time]
+look_luce[, ss := scaler(maxact), by = time]
+
+
+
+
+
 
 trace[, maxact := max(targetact, cohortact, rhymeact), by = .(simulation, frame)]
 trace[, ss := scaler(maxact)]
--- data/bob_trace_data/prep_bob_data.R
+++ data/bob_trace_data/prep_bob_data.R
@@ -1,22 +1,53 @@
 ## This requires two files that are located in my onedrive but too larget
 # to keep in git repo
+setwd("~/dissertation/data/bob_trace_data/")
+# ## This is on onedrive
+# dt <- fread("looks-matched_human.txt", fill = TRUE) #218 mb
+# dt[, `:=`(trial = NULL, lookID = NULL, StartTime = NULL,
+#           None = NULL, NoData = NULL, `vstim-Unrelated1` = NULL,
+#           `vstim-Unrelated2` = NULL, `vstim-Unrelated3` = NULL,
+#           `vstim-Unrelated4` = NULL, `vstim-Rhyme` = NULL)] # things i didn't need to download (72mb)
+# 
+# dt[, `:=`(target = mean(`vstim-Target`, na.rm = TRUE),
+#           cohort = mean(`vstim-Cohort`, na.rm = TRUE)), by = .(subject, time)]
+# 
+# dt[, `:=`(`vstim-Target` = NULL, `vstim-Cohort` = NULL,
+#           saccadenum = NULL)]
+# dt <- unique(dt)
+# 
+# fwrite(dt, file = "human_looks.csv")
+looks <- fread("../trace_n_things/looks-matched_human.txt")
 
-## This is on onedrive
-dt <- fread("looks-matched_human.txt", fill = TRUE) #218 mb
-dt[, `:=`(trial = NULL, lookID = NULL, StartTime = NULL,
-          None = NULL, NoData = NULL, `vstim-Unrelated1` = NULL,
-          `vstim-Unrelated2` = NULL, `vstim-Unrelated3` = NULL,
-          `vstim-Unrelated4` = NULL, `vstim-Rhyme` = NULL)] # things i didn't need to download (72mb)
 
-dt[, `:=`(target = mean(`vstim-Target`, na.rm = TRUE),
-          cohort = mean(`vstim-Cohort`, na.rm = TRUE)), by = .(subject, time)]
+## Get rid of non-regular subjects
+sub <- fread("../trace_n_things/subject_protocol.txt")
+sub <- sub[Protocol == "N", ] # 40 of them
+looks <- looks[subject %in% sub$subjectID, ]
 
-dt[, `:=`(`vstim-Target` = NULL, `vstim-Cohort` = NULL,
-          saccadenum = NULL)]
-dt <- unique(dt)
+looks[, `:=`(unrel= sum(`vstim-Unrelated1`, `vstim-Unrelated2`, `vstim-Unrelated3`, `vstim-Unrelated4`)), 
+      by = .(trial, time)]
 
-fwrite(dt, file = "human_looks.csv")
 
+## Get mean value of each activation by subject
+looks[, `:=`(target = mean(`vstim-Target`, na.rm = TRUE),
+             cohort = mean(`vstim-Cohort`, na.rm = TRUE), 
+             unrelated = mean(unrel, na.rm = TRUE), 
+             rhyme = mean(`vstim-Rhyme`, na.rm = TRUE)), 
+      by = .(subject, time)]
+
+## Remove those I don't want
+looks[, `:=`(lookID = NULL, StartTime = NULL, 
+             None = NULL, NoData = NULL, saccadenum = NULL, 
+             `vstim-Unrelated1` = NULL,
+             `vstim-Unrelated2` = NULL, `vstim-Unrelated3` = NULL,
+             `vstim-Unrelated4` = NULL, `vstim-Rhyme` = NULL, 
+             `vstim-Target` = NULL, `vstim-Cohort` = NULL, unrel,
+             trial = NULL)]
+
+## Only need one set of time points for each subject (since averaged across trials)
+looks <- unique(looks)
+
+fwrite(looks, "human_looks.csv")
 
 ## Also on onedrive
 dts <- fread("Events-matched-sac-fix.txt")
@@ -40,9 +71,29 @@ fwrite(dts, file = "human_saccades.csv")
 
 
 ## Finally, lets look at trace
-ddt <- fread("trace_query.txt")
-ddt[, `:=`(simulation = NULL, rhymeact = NULL)]
-ddt[, `:=`(target = mean(targetact), cohort = mean(cohortact)), by = frame]
-ddt[, `:=`(targetact = NULL, cohortact = NULL)]
-ddt <- unique(ddt)
-fwrite(ddt, file = "trace_curves.csv")
+# ddt <- fread("trace_query.txt")
+# ddt[, `:=`(simulation = NULL, rhymeact = NULL)]
+# ddt[, `:=`(target = mean(targetact), cohort = mean(cohortact)), by = frame]
+# ddt[, `:=`(targetact = NULL, cohortact = NULL)]
+# ddt <- unique(ddt)
+
+trace <- fread("../trace_n_things/trace_query.txt")
+
+## Take mean across simulations
+trace[, `:=`(target = mean(targetact), 
+             cohort = mean(cohortact), 
+             rhyme = mean(rhymeact), 
+             ur = mean(uract)), by = .(frame)]
+
+## Time is 16 * frame + 75
+trace[, time := 16 * frame + 75]
+
+## Remove unnecessary vars
+trace[, `:=`(simulation = NULL, targetact = NULL, 
+             cohortact = NULL, rhymeact = NULL, 
+             uract = NULL, frame = NULL)]
+
+## Unique only
+trace <- unique(trace)
+
+fwrite(trace, file = "trace_curves.csv")

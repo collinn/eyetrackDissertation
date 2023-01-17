e92e5b9c6a53707d4946ad6481423e7ccf7ed09f
collin <collin@uiowa.edu>
2023-01-16 18:43:25 -0600

    trace games

--- analysis/trace_data_prep.R
+++ analysis/trace_data_prep.R
@@ -22,17 +22,17 @@ tsims <- tsims[idx]
 trace <- trace[simulation %in% tsims, ]
 
 ## Take mean across simulations
-trace[, `:=`(target = mean(targetact), 
-             cohort = mean(cohortact), 
-             rhyme = mean(rhymeact), 
+trace[, `:=`(target = mean(targetact),
+             cohort = mean(cohortact),
+             rhyme = mean(rhymeact),
              ur = mean(uract)), by = .(frame)]
 
 ## Time is 16 * frame + 75
 trace[, time := 16 * frame + 75]
 
 ## Remove unnecessary vars
-trace[, `:=`(simulation = NULL, targetact = NULL, 
-             cohortact = NULL, rhymeact = NULL, 
+trace[, `:=`(simulation = NULL, targetact = NULL,
+             cohortact = NULL, rhymeact = NULL,
              uract = NULL, frame = NULL)]
 
 ## Unique only
@@ -54,7 +54,7 @@ scale_trace <- function(tau = 4.5) {
     den <- (1 + w * exp(-s * (a - cc)))^(1/w)
     (ss <- (p-b)/den + b)
   }
-  
+
   # Range tau = 2-4.5
   lucer <- function(l, tau = 4.5) {
     expl <- lapply(l, function(x) {
@@ -64,46 +64,46 @@ scale_trace <- function(tau = 4.5) {
     rr <- lapply(expl, function(x) as.data.table(x / ss))
     rr
   }
-  
+
   ## Implement luce
   l <- as.list(trace[, -"time", with = FALSE])
   trace_luce <- Reduce(`cbind`, (lucer(l, tau)))
   names(trace_luce) <- names(l)
   trace_luce <- cbind(trace[, .(time)], trace_luce)
-  
+
   #trace_luce <- trace
-  
+
   ## Compute scaling term, using both p/b and 0/1
   # for target specifically
   bb_t <- trace_luce[1, target] # first
   pp_t <- trace_luce[108, target] # last
-  
+
   ## Once using the correct p/b
   trace_luce[, ss_targ := scaler(max(target, cohort, rhyme, ur), bb_t, pp_t), by = time]
   trace_luce[, ss_targ2 := scaler(max(target, cohort, rhyme, ur), 0, 1), by = time]
-  
-  trace_luce[, `:=`(targ_bp = target * ss_targ, 
+
+  trace_luce[, `:=`(targ_bp = target * ss_targ,
                     targ_nobp = target * ss_targ2)]
-  
+
   ###############
   ## COHORT #####
   ###############
-  
+
   bb_c <- trace_luce[1, cohort] # first
   pp_c <- trace_luce[108, cohort] # last
-  
+
   ## Once using the correct p/b
   trace_luce[, ss_co := scaler(max(target, cohort, rhyme, ur), bb_c, pp_c), by = time]
   trace_luce[, ss_co2 := scaler(max(target, cohort, rhyme, ur), 0, 1), by = time]
-  
-  trace_luce[, `:=`(cohort_bp = cohort * ss_co, 
+
+  trace_luce[, `:=`(cohort_bp = cohort * ss_co,
                     cohort_nobp = cohort * ss_co2)]
-  
-  
+
+
   ## Get rid of intermediate pieces
-  trace_luce[, `:=`(ss_targ = NULL, 
-                    ss_targ2 = NULL, 
-                    ss_co = NULL, 
+  trace_luce[, `:=`(ss_targ = NULL,
+                    ss_targ2 = NULL,
+                    ss_co = NULL,
                     ss_co2 = NULL)]
   trace_luce
 }
@@ -116,7 +116,96 @@ aa <- scale_trace(tau = 4.5)
 bb <- scale_trace(tau = 3)
 cc <- scale_trace(tau = 3.5)
 dd <- scale_trace(tau = 4)
+ee <- scale_trace(tau = 7)
 fwrite(aa, "~/dissertation/data/bob_trace_data/trace_scaled_4.5.csv")
 fwrite(bb, "~/dissertation/data/bob_trace_data/trace_scaled_3.csv")
 fwrite(cc, "~/dissertation/data/bob_trace_data/trace_scaled_3.5.csv")
 fwrite(dd, "~/dissertation/data/bob_trace_data/trace_scaled_4.csv")
+fwrite(ee, "~/dissertation/data/bob_trace_data/trace_scaled_7.csv")
+
+
+## But now try with sigmoid instead of fixed tau
+
+scale_trace_sigmoid <- function(pp = NULL) {
+  trace <- fread("~/dissertation/data/bob_trace_data/trace_curves.csv")
+  scaler <- function(a, b = 0, p = 1) { #a is max activation
+    s <- 4
+    w <- 0.0001
+    cc <- 0.25
+    den <- (1 + w * exp(-s * (a - cc)))^(1/w)
+    (ss <- (p-b)/den + b)
+  }
+
+  # Range tau = 2-4.5
+  lucer <- function(l, ttt) {
+
+    # temperature parmeters
+    if (is.null(pp)) {
+      temp_pars <- c(2, 4.5, 0.004, 1000)
+    } else {
+      temp_pars <- pp
+    }
+
+    temp <- eyetrackSim:::logistic_f(p = temp_pars, t = ttt)
+
+    expl <- lapply(l, function(x) {
+      exp(temp * x)
+    })
+    ss <- Reduce(`+`, expl)
+    rr <- lapply(expl, function(x) as.data.table(x / ss))
+    rr
+  }
+
+  ## Implement luce
+  TRACE_TIME <- unique(trace$time)
+  l <- as.list(trace[, -"time", with = FALSE])
+  trace_luce <- Reduce(`cbind`, (lucer(l, TRACE_TIME)))
+  names(trace_luce) <- names(l)
+  trace_luce <- cbind(trace[, .(time)], trace_luce)
+
+  #trace_luce <- trace
+
+  ## Compute scaling term, using both p/b and 0/1
+  # for target specifically
+  bb_t <- trace_luce[1, target] # first
+  pp_t <- trace_luce[108, target] # last
+
+  ## Once using the correct p/b
+  trace_luce[, ss_targ := scaler(max(target, cohort, rhyme, ur), bb_t, pp_t), by = time]
+  trace_luce[, ss_targ2 := scaler(max(target, cohort, rhyme, ur), 0, 1), by = time]
+
+  trace_luce[, `:=`(targ_bp = target * ss_targ,
+                    targ_nobp = target * ss_targ2)]
+
+  ###############
+  ## COHORT #####
+  ###############
+
+  bb_c <- trace_luce[1, cohort] # first
+  pp_c <- trace_luce[108, cohort] # last
+
+  ## Once using the correct p/b
+  trace_luce[, ss_co := scaler(max(target, cohort, rhyme, ur), bb_c, pp_c), by = time]
+  trace_luce[, ss_co2 := scaler(max(target, cohort, rhyme, ur), 0, 1), by = time]
+
+  trace_luce[, `:=`(cohort_bp = cohort * ss_co,
+                    cohort_nobp = cohort * ss_co2)]
+
+
+  ## Get rid of intermediate pieces
+  trace_luce[, `:=`(ss_targ = NULL,
+                    ss_targ2 = NULL,
+                    ss_co = NULL,
+                    ss_co2 = NULL)]
+  trace_luce
+}
+
+
+aa <- scale_trace(tau = 4.5)
+bb <- scale_trace(tau = 3)
+xx <- scale_trace_sigmoid()
+
+xx2 <- scale_trace_sigmoid(c(2, 4.5, 0.002, 600))
+
+fwrite(xx, "~/dissertation/data/bob_trace_data/trace_sigmoid.csv")
+fwrite(xx2, "~/dissertation/data/bob_trace_data/trace_sigmoid2.csv")
--- writing/saccade/scripts/comparing_trace.R
+++ writing/saccade/scripts/comparing_trace.R
@@ -23,42 +23,43 @@ looks[, group := "A"]
 sacs[, group := "A"]
 
 
+
 #sacs <- sacs[starttime > 0, ]
 
 
 if (file.exists("~/dissertation/data/saccade_look_fits/target_fits_bdots.RData")) {
   load("~/dissertation/data/saccade_look_fits/target_fits_bdots.RData")
 } else {
-  fit_looks <- bdotsFit(data = looks, 
-                        subject = "subject", 
-                        time = "time", 
-                        y = "target", 
-                        group = "group", 
+  fit_looks <- bdotsFit(data = looks,
+                        subject = "subject",
+                        time = "time",
+                        y = "target",
+                        group = "group",
                         curveType = logistic())
-  
-  fit_looks_rt <- bdotsFit(data = looksrt, 
-                         subject = "subject", 
-                         time = "time", 
-                         y = "target", 
-                         group = "group", 
+
+  fit_looks_rt <- bdotsFit(data = looksrt,
+                         subject = "subject",
+                         time = "time",
+                         y = "target",
+                         group = "group",
                          curveType = logistic())
-  
+
   #sacs <- sacs[starttime <= 2000, ]
   ## logistic2() at bottom of this script
-  fit_sacs  <- bdotsFit(data = sacs, 
-                        subject = "subject", 
-                        time = "starttime", 
-                        y = "target", 
-                        group = "group", 
-                        curveType = logistic(c(mini = 0, peak = 1, slope = 0.002, cross = 750)))  
-  fit_sacs_rt  <- bdotsFit(data = sacsrt, 
-                         subject = "subject", 
-                         time = "starttime", 
-                         y = "target", 
-                         group = "group", 
-                         curveType = logistic(c(mini = 0, peak = 1, slope = 0.002, cross = 750)))  
-  
-  save(fit_looks, fit_looks_rt, fit_sacs, fit_sacs_rt, 
+  fit_sacs  <- bdotsFit(data = sacs,
+                        subject = "subject",
+                        time = "starttime",
+                        y = "target",
+                        group = "group",
+                        curveType = logistic(c(mini = 0, peak = 1, slope = 0.002, cross = 750)))
+  fit_sacs_rt  <- bdotsFit(data = sacsrt,
+                         subject = "subject",
+                         time = "starttime",
+                         y = "target",
+                         group = "group",
+                         curveType = logistic(c(mini = 0, peak = 1, slope = 0.002, cross = 750)))
+
+  save(fit_looks, fit_looks_rt, fit_sacs, fit_sacs_rt,
        file = "~/dissertation/data/saccade_look_fits/target_fits_bdots.RData")
 }
 
@@ -69,7 +70,7 @@ if (file.exists("~/dissertation/data/saccade_look_fits/target_fits_bdots.RData")
 idxrm0 <- which(fit_looks$fitCode >= 5)
 idxrm1 <- which(fit_looks_rt$fitCode >= 5)
 
-# Remove these from saccades 
+# Remove these from saccades
 qq <- coef(fit_sacs)
 idxrm2 <- which(qq[,3] < 0 | qq[,4] < 0 | qq[,1] > qq[,2])
 
@@ -83,10 +84,10 @@ idxrm <- Reduce(union, list(idxrm0, idxrm2)) # no need for RT stuff here
 idx <- setdiff(1:40, idxrm)
 
 fit_looks <- fit_looks[idx, ]
-fit_looks_rt <- fit_looks_rt[idx, ]
+#fit_looks_rt <- fit_looks_rt[idx, ]
 
 fit_sacs <- fit_sacs[idx, ]
-fit_sacs_rt <- fit_sacs_rt[idx, ]
+#fit_sacs_rt <- fit_sacs_rt[idx, ]
 
 mm1 <- colMeans(coef(fit_looks))
 mm2 <- colMeans(coef(fit_looks_rt))
@@ -95,25 +96,34 @@ mm4 <- colMeans(coef(fit_sacs_rt))
 
 time <- 0:1787
 f_fix <- logistic_f(mm1, time)
+f_fix_rt <- logistic_f(mm2, time)
 f_sac <- logistic_f(mm3, time)
+f_sac_rt <- logistic_f(mm4, time)
 
 ## Pretty sure I figured out trace (4 looks best)
 #trace_luce <- fread("~/dissertation/data/bob_trace_data/trace_scaled_4.5.csv")
 #trace_luce <- fread("~/dissertation/data/bob_trace_data/trace_scaled_3.csv")
 #trace_luce <- fread("~/dissertation/data/bob_trace_data/trace_scaled_3.5.csv")
 trace_luce <- fread("~/dissertation/data/bob_trace_data/trace_scaled_4.csv")
+#trace_luce <- fread("~/dissertation/data/bob_trace_data/trace_scaled_7.csv")
+trace_sigmoid <- fread("~/dissertation/data/bob_trace_data/trace_sigmoid.csv")
+trace_sigmoid2 <- fread("~/dissertation/data/bob_trace_data/trace_sigmoid2.csv")
 
 ## OR in ggplot
 dt1 <- data.table(time = time, y = f_fix, Method = "Fixation")
 dt2 <- data.table(time = time, y = f_sac, Method = "Saccade")
+# dt5 <- data.table(time = time, y = f_fix_rt, Method = "Fixation_RT")
+# dt6 <- data.table(time = time, y = f_sac_rt, Method = "Saccade_RT")
 dt3 <- data.table(time = trace_luce$time, y = trace_luce$targ_bp, Method = "TRACE")
+dt4 <- data.table(time = trace_luce$time, y = trace_sigmoid$targ_bp, Method = "trace_sigmoid")
+dt5 <- data.table(time = trace_luce$time, y = trace_sigmoid2$targ_bp, Method = "trace_sigmoid2")
 
-dt <- rbindlist(list(dt1, dt2, dt3))
+dt <- rbindlist(list(dt1, dt2, dt3, dt4, dt5))
 
-pdf("../img/sac_fix_trace_compare.pdf", 
+pdf("../img/sac_fix_trace_compare.pdf",
     width = 5.5, height = 4)
-ggplot(dt, aes(x = time, y = y, color = Method)) + 
-  geom_line(linewidth = 1.5) + ylim(c(min(dt1$y),1)) + theme_bw() +
+ggplot(dt, aes(x = time, y = y, color = Method)) +
+  geom_line() + ylim(c(min(dt1$y),1)) + theme_bw() +
   ggtitle("Comparison of Fixation/Saccade\nMethods with TRACE") +
   labs(y = expression(f[theta](t)), x = "Time")
 dev.off()
@@ -123,8 +133,10 @@ dev.off()
 library(xtable)
 
 
-TRACE_fun <- approxfun(x = trace_luce$time, 
+TRACE_fun <- approxfun(x = trace_luce$time,
                        y = trace_luce$targ_bp)
+TRACE_fun_sigmoid <- approxfun(x = trace_sigmoid$time,
+                       y = trace_sigmoid2$targ_bp)
 
 getRmvIdx <- function(ff) {
   rr <- coef(ff)
@@ -132,37 +144,35 @@ getRmvIdx <- function(ff) {
   idx
 }
 
-mise <- function(pars) {
-  times <- 100:101
-  
-  if (is.matrix(pars)) {
-    pars <- split(pars, 1:nrow(pars))
-    mv <- vapply(pars, function(x) {
-      g <- function(tt) {
-        (logistic_f(x, tt) - TRACE_fun(tt))^2
-      }
-      integrate(g, lower = min(times), upper = max(times))$value
-    }, 1)
-  } else {
+mise <- function(pars, f, RMS = TRUE) {
+  times <- seq(75, 1787, by = 10)
+
+  pars <- split(pars, 1:nrow(pars))
+  mv <- vector("numeric", length = length(pars))
+
+  gg <- function(p) {
     g <- function(tt) {
-      (logistic_f(pars, tt) - TRACE_fun(tt))^2
+      (logistic_f(p, tt) - f(tt))^2
     }
-    mv <- integrate(g, lower = min(times), upper = max(times))$value
+  }
+
+  if (!RMS) {
+    for (i in seq_along(mv)) {
+      g <- gg(pars[[i]])
+      mv[i] <- tryCatch({integrate(g, lower = min(times), upper = max(times))$value}, error = function(e) NA)
+    }
+  } else {
+    for (i in seq_along(mv)) {
+      g <- gg(pars[[i]])
+      mv[i] <- sqrt(sum(g(times))/length(mv))
+    }
   }
   mv
 }
 
-## These already subset above
-mm1 <- colMeans(coef(fit_looks))
-mm3 <- colMeans(coef(fit_sacs))
-
 ## Do just mean first
-mise(mm1)
-rr <- mise(coef(fit_looks))
-
-
-mise(mm3)
-ss <- mise(coef(fit_sacs))
+rr <- mise(coef(fit_looks), TRACE_fun)
+ss <- mise(coef(fit_sacs), TRACE_fun)
 
 
 ss1 <- setnames(transpose(data.table(as.numeric(summary(rr)))), names(summary(rr)))
@@ -172,3 +182,20 @@ nn <- data.table(Method = c("Fixation", "Saccade"))
 ss <- cbind(nn, rbindlist(list(ss1, ss2)))
 
 print(xtable(ss, digits = 8), include.rownames = FALSE)
+
+
+## And again with sigmoid
+rr_sig <- mise(coef(fit_looks), TRACE_fun_sigmoid)
+ss_sig <- mise(coef(fit_sacs), TRACE_fun_sigmoid)
+
+
+ss1_sig <- setnames(transpose(data.table(as.numeric(summary(rr_sig)))), names(summary(rr_sig)))
+ss2_sig <- setnames(transpose(data.table(as.numeric(summary(ss_sig)))), names(summary(ss_sig)))
+
+nn_sig <- data.table(Method = c("Fixation (Sigmoid)", "Saccade (Sigmoid)"))
+ss_sig <- cbind(nn_sig, rbindlist(list(ss1_sig, ss2_sig)))
+
+
+
+
+

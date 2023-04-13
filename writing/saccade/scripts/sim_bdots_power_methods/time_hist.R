ae5a05d3ab99919c0dc970d187f75465dbc5caab
collin <collin@uiowa.edu>
2023-04-13 14:40:35 -0500

    table

--- writing/saccade/scripts/sim_bdots_power_methods/time_hist.R
+++ writing/saccade/scripts/sim_bdots_power_methods/time_hist.R
@@ -157,54 +157,81 @@ dev.off()
 
 
 
+########## Table of correlations between error and hist?
+
+timeDiffCorr <- function(idx) {
+  y <- readRDS(ff[idx])
+  ons <- y[1, ]
+  fix <- y[2, ]
+
+  timesig <- function(mm) {
+    time <- seq(0, 2000, 4)
+    vec <- vector("numeric", length = length(time))
+
+    if (is.null(dim(mm))) {
+      vec <- as.logical(vec)
+      return(vec)
+    }
+
+    sm <- split(mm, row(mm))
+    bv <- lapply(sm, function(m) {
+      sigt <- do.call(seq, as.list(c(m, 4)))
+    })
+    bv <- Reduce(union, bv)
+    rr <- time %in% bv # logical vec
+    return(rr)
+  }
+
+  TIME <- seq(0, 2000, 4)
+
+  ons_mat <- sapply(ons, timesig)
+  ons_mat <- apply(ons_mat, 2, function(z) TIME[z])
+  ons_mat <- Reduce("c", ons_mat)
+
+  fix_mat <- sapply(fix, timesig)
+  fix_mat <- apply(fix_mat, 2, function(z) TIME[z])
+  fix_mat <- Reduce("c", fix_mat)
+
+  s1 <- hist(ons_mat, breaks = seq(0, 2000, 40), plot = FALSE)
+  s2 <- hist(fix_mat, breaks = seq(0, 2000, 40), plot = FALSE)
 
 
+  dat <- data.table(Method = rep(c("Onset", "Proportion"),
+                                 times = c(length(ons_mat),
+                                           length(fix_mat))),
+                    Time = c(ons_mat, fix_mat))
+
+  ## Get density from each hist
+  ons_den <- s1$density
+  prop_den <- s2$density
+
+  if (idx %in% 1:3) {
+    dd <- diffl$y[diffl$Time %in% s1$breaks[-1]]
+  } else {
+    dd <- diffg$y[diffl$Time %in% s1$breaks[-1]]
+  }
+  dd <- abs(dd)
+
+  cv <- ifelse(idx %in% 1:3, "Logistic", "Asymmetric Gauss")
+  dll <- ifelse(idx %in% c(1,4), "No Delay",
+                ifelse(idx %in% c(2,5), "Normal Delay", "Weibull Delay"))
+  val <- c(ons_cor = cor(ons_den, dd), prop_cor = cor(prop_den, dd))
+
+  c(Curve = cv, Delay = dll, round(val, 4))
+
+}
+
+ff <- list.files("rds_files", full.names = TRUE, pattern = "rds")
+
+tab <- sapply(1:6, timeDiffCorr) |> t()
+
+xtable::xtable(tab) |> print(include.rownames = FALSE)
 
 
+ p1 <- powerHist(1, "Logistic, No Delay")
+p2 <- powerHist(2, "Logistic, Normal Delay")
+p3 <- powerHist(3, "Logistic, Weibull Delay")
 
-# y <- ff[1]
-## With logistic data
-ggplot(dat, aes(x = Time, color = Method, fill = Method)) +
-  geom_histogram(alpha = 0.5, position = "identity", binwidth = 40) +
-  ylab("Frequency") + theme_bw() + scale_fill_manual(values = c("#619CFF", "#00BA38")) +
-  scale_color_manual(values = c("#619CFF", "#00BA38")) + ggtitle(tit) +
-  geom_line(data = diffl, aes(x = Time, y = abs(y*10000)), color = "red", linewidth = 1) +
-  scale_y_continuous(
-    # Features of the first axis
-    name = "Frequency",
-    # Add a second axis and specify its features
-    sec.axis = sec_axis( trans=~.*.00001, name="Absolute Difference")
-  )
-
-# y <- ff[4]
-## dg
-dat$Method <- factor(dat$Method, levels = c("Onset", "Proportion", "Difference"))
-ggplot(dat, aes(x = Time, color = Method, fill = Method)) +
-  geom_histogram(alpha = 0.5, position = "identity", binwidth = 40) +
-  ylab("Frequency") + theme_bw() + scale_fill_manual(values = c("#619CFF", "#00BA38")) +
-  scale_color_manual(values = c("#619CFF", "#00BA38")) + ggtitle(tit) +
-  geom_line(data = diffg, aes(x = Time, y = abs(y*10000), col = Method), linewidth = 1) +
-  scale_y_continuous(
-    # Features of the first axis
-    name = "Frequency",
-    # Add a second axis and specify its features
-    sec.axis = sec_axis( trans=~.*.00001, name="Absolute Difference")
-  ) +
-  scale_color_manual(values = c(Onset = "red", Proportion = "green", Difference = "black"))
-
-
-
-
-
-
-
-ggplot(dat, aes(x = Time, color = Method)) +
-  geom_histogram(alpha = 0, position = "identity", binwidth = 40) +
-  ylab("Frequency") + theme_bw() + ggtitle(tit) +
-  geom_line(data = diffl, aes(x = Time, y = abs(y*10000), col = Method), linewidth = 1) +
-  scale_y_continuous(
-    # Features of the first axis
-    name = "Frequency",
-    # Add a second axis and specify its features
-    sec.axis = sec_axis( trans=~.*.00001, name="Absolute Difference")
-  )
+p4 <- powerHist(4, "Asymmetric Gauss, No Delay")
+p5 <- powerHist(5, "Asymmetric Gauss, Normal Delay")
+p6 <- powerHist(6, "Asymmetric Gauss, Weibull Delay")
